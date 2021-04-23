a8ebb15a633b43137ceb01155cd75b3c6a1745c1
arjun-myanger <admyanger@outlook.com>
2021-04-23 20:14:02 +0100

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import SpotifyWebApi from 'spotify-web-api-js';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import Player from './Player';
+
+const spotify = new SpotifyWebApi();
+
 
 function App() {
   const [token, setToken] = useState(null);
-
-
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -16,6 +19,12 @@ function App() {
 
     if (_token) {
       setToken(_token)
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log('person', user);
+      });
     }
 
   }, [])
@@ -24,7 +33,7 @@ function App() {
     <div className="app">
       {
         token ? (
-          <h1>I am logged in</h1>
+          <Player />
         ) : (
           <Login />
         )
