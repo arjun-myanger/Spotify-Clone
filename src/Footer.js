a281c1778565e351b3d9c0f79443f07f64391d62
arjun-myanger <admyanger@outlook.com>
2021-04-25 02:00:49 +0100

    next commit

--- src/Footer.js
+++ src/Footer.js
@@ -1,23 +1,48 @@
 import React from 'react';
 import './Footer.css';
-
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import { VolumeDown } from '@material-ui/icons';
+import { Grid, Slider } from '@material-ui/core';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
 
 
 function Footer() {
     return (
         <div className='footer'>
             <div className="footer__left">
-                <h1>Album and Song details</h1>
+                <img className="footer__albumLogo" src="https://images-na.ssl-images-amazon.com/images/I/81ex1HcXOPL._SL1280_.jpg" alt="" />
+                <div className="footer__songInfo">
+                    <h4>Viva La Vida</h4>
+                    <p>Coldplay</p>
+                </div>
             </div>
 
             <div className="footer__center">
-                <h1>Player control</h1>
+                <ShuffleIcon className='footer__green' />
+                <SkipPreviousIcon className='footer__icon' />
+                <PlayCircleOutlineIcon fontSize='large' className='footer__icon' />
+                <SkipNextIcon className='footer__icon' />
+                <RepeatIcon className='footer__green' />
             </div>
 
             <div className="footer__right">
-                <p>Volume</p>
-            </div>
-        </div>
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDown />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
+            </div >
+        </div >
     )
 }
 
